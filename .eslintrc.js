d8b7b115acac979a0dd39f38bedf4c28dea24e6c
Kay <kard@hotmail.co.uk>
2022-07-28 10:11:54 +0100

    Added eslint and configured jest environment

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "jest": true,
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
--- serverModel.test.js
+++ serverModel.test.js
@@ -2,25 +2,25 @@ const ServerModel = require('./serverModel')
 
 describe('ServerModel',() => {
   it('saves a key value pair and returns the value when given the key',() => {
-    model = new ServerModel
+    const model = new ServerModel
     model.add({'animal':'cat'})
     expect(model.retrieve('animal')).toEqual('cat');
   })
 
   it('saves a different key value pair and returns the value',() => {
-    model = new ServerModel
+    const model = new ServerModel
     model.add({'pet':'dog'})
     expect(model.retrieve('pet')).toEqual('dog');
   })
 
   it('returns a different value for animal',() => {
-    model = new ServerModel
+    const model = new ServerModel
     model.add({'animal':'dog'})
     expect(model.retrieve('animal')).toEqual('dog');
   })
 
   it('saves multiple key value pairs',() => {
-    model = new ServerModel
+    const model = new ServerModel
     model.add({'animal':'cat'})
     model.add({'pet':'dog'})
     expect(model.retrieve('animal')).toEqual('cat');
@@ -28,7 +28,7 @@ describe('ServerModel',() => {
   })
 
   it('overwrites a previous key-value pair',() => {
-    model = new ServerModel
+    const model = new ServerModel
     model.add({'animal':'cat'})
     model.add({'animal':'fish'})
     expect(model.retrieve('animal')).toEqual('fish')
