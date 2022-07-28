c81db99f2f649702a6b0d7afe91f27c7a5958234
Kay <kard@hotmail.co.uk>
2022-07-28 09:39:00 +0100

    Server stores multiple key value pairs and returns each

--- features/set_a_key_value_pair.test.js
+++ features/set_a_key_value_pair.test.js
@@ -20,4 +20,25 @@ describe('server',() => {
         console.error(error);
       }));
   })
+
+  it('creates multiple key value pairs and retrieves their value',() => {
+    fetch('http://localhost:4000/set?pet=dog')
+      .then(
+        fetch('http://localhost:4000/set?animal=cat'))
+          .then(
+            fetch('http://localhost:4000/get?key=animal')
+            .then(response => response.json())
+            .then(data => expect(data).toEqual('cat'))
+            .catch((error) => { 
+              console.error(error);
+            })
+          .then(
+            fetch('http://localhost:4000/get?key=pet')
+            .then(response => response.json())
+            .then(data => expect(data).toEqual('dog'))
+            .catch((error) => { 
+              console.error(error);
+            })
+          ));
+  });
 })
