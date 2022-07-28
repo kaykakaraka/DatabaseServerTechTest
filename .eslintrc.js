faf155f89bc7d25728112a564807a4fac7f8f3eb
Kay <kard@hotmail.co.uk>
2022-07-28 10:16:13 +0100

    Added configuration for commonjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
     "env": {
         "jest": true,
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "commonjs": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest",
-        "sourceType": "module"
+        "sourceType": "script"
     },
     "rules": {
     }
